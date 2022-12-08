8513d797679d69f930e9371bdbd6ad388b8b58ac
Xigmah <razn.abuqamar93@gmail.com>
2022-12-07 23:37:03 -0800

    First go

---
+++ helloworld/lib/hello_world.h
@@ -0,0 +1,20 @@
+/**
+ * @file hello_world.h
+ *
+ * @brief Header file for hello_world.cpp
+ *
+ * @author Razn I. Abu Qamar
+ *
+ */
+
+#ifndef HELLO_WORLD_H
+#define HELLO_WORLD_H
+
+namespace World{
+    class TestClass {
+        public:
+            int main();
+    };
+}
+
+#endif //HELLO_WORLD_H
---
+++ helloworld/src/hello_world.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file hello_world.cpp
+ *
+ * @brief Must do first program for any language
+ *
+ * @author Razn I. Abu Qamar
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <lib/hello_world.h>
+
+using namespace World;
+
+int TestClass::main(){
+    std::cout << "Hello World!";
+    return 0;
+};
